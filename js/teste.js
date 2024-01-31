e78320ea7490931b1c4d1e149ce5e053bfc5fdf0
MahAzevedo <madavetto@gmail.com>
2024-01-31 02:09:06 -0300

    teste

--- js/teste.js
+++ js/teste.js
@@ -709,3 +709,19 @@ console.log(quadrado.area(15));
 console.log(quadrado.perimetro(100));
 
 // typeof diz que tipo que é, se é string ou número
+
+var menu = {
+    width: 800,
+    height: 50,
+    backgroundColor: '#84E',
+}
+
+menu.backgroundColor = '#000';
+
+menu.color = 'blue';
+
+menu.esconder = function () {
+    console.log('Escondi');
+}
+
+var bg = menu.backgroundColor;
